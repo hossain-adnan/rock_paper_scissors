edcce412b7242a6bdc523bae6d827776ff8b8c44
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-09-24 18:49:51 +0600

    Turn psuedocode into code to get the computer choice

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ console.log('Hello Wprld');
 
 // Logic to get the computer choice
 
-// START
+function getComputerChoice() {
 
-    // Create a new function named getComputerChoice.
-    // getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+    let getNum = Math.floor(Math.random() * 3);
 
-        // Use Math.random() to return 3 intergers 0,1,2 to represent “rock”, “paper” & “scissors”
-        // FINDOUT: How to return 3 integers with Math.random()
-        // IF 0 THEN 
-            // print “rock”
-        // ELSE ...
+    if (getNum === 0) {
+        return "rock";
+    } else if (getNum === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 
-// END
+console.log(getComputerChoice);
+
 
