b7d2b474f7f464bf0f70da0ffe77c988ce96c14e
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-09-24 13:16:55 +0600

    Write initial psuedocode to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello Wprld');
+
+// Logic to get the computer choice
+
+// START
+
+    // Create a new function named getComputerChoice.
+    // getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+
+        // Use Math.random() to return 3 intergers 0,1,2 to represent “rock”, “paper” & “scissors”
+        // FINDOUT: How to return 3 integers with Math.random()
+        // IF 0 THEN 
+            // print “rock”
+        // ELSE ...
+
+// END
+
