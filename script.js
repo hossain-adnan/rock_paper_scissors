7a7979024e31d6acc6252d5d5ad66ad665ce71ee
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-10-09 11:19:50 +0600

    Complete Game logic

--- script.js
+++ script.js
@@ -2,6 +2,25 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const scoreboard = document.querySelector('.scoreboard');
+scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
+
+function checkScore() {
+    let winner = document.createElement('span')
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore > computerScore) {
+            winner.textContent = 'You Win! ';
+            scoreboard.prepend(winner);
+        } else {
+            winner.textContent = 'The Machine Wins! ';
+            scoreboard.prepend(winner);
+        }
+        humanScore = 0;
+        computerScore = 0;
+    } 
+}
+
 // Logic to get the computer choice
 function getComputerChoice() {
 
@@ -16,22 +35,25 @@ function getComputerChoice() {
     }
 }
 
-// Logic to get the human choice
+// Logic to get the human choice & play 
 
 const rockBtn = document.querySelector("#rock")
 rockBtn.addEventListener("click", () => {
-    playRound('rock', getComputerChoice()) 
+    playRound('rock', getComputerChoice());
+    checkScore(); 
 });
 
 
 const paperBtn = document.querySelector("#paper")
 paperBtn.addEventListener("click", () => {
-    playRound('paper', getComputerChoice())
+    playRound('paper', getComputerChoice());
+    checkScore();
 });
 
 const scissorsBtn = document.querySelector("#scissors")
 scissorsBtn.addEventListener("click", () => {
-    playRound('scissors', getComputerChoice())
+    playRound('scissors', getComputerChoice());
+    checkScore();
 });
 
 const divList = document.querySelector('ul'); // Div to show text
@@ -44,46 +66,64 @@ function playRound(humanChoice, computerChoice) {
 
     choiceList.textContent = `You: ${humanChoice} | Computer: ${computerChoice}`;
     divList.appendChild(choiceList);
-    // divList.removeChild(roundResult);  Doesn't work   
 
     if (humanChoice === 'rock') {
         if (computerChoice == 'scissors') {
             humanScore += 1;
             roundResult.textContent = `You win! Because ${humanChoice} beats ${computerChoice}`;
             divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
 
         } else if (computerChoice == 'paper') {
             computerScore += 1;
             roundResult.textContent = `You lose! Because ${computerChoice} beats ${humanChoice}`;
             divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
 
         } else {
             roundResult.textContent = "It's a tie";
             divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
         }
-    }
+    } //this far
 
     else if (humanChoice === 'paper') {
         if (computerChoice == 'rock') {
             humanScore += 1;
-            console.log(`You win! Because ${humanChoice} beats ${computerChoice}`);
+            roundResult.textContent =`You win! Because ${humanChoice} beats ${computerChoice}`;
+            divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
         } else if (computerChoice == 'scissors') {
             computerScore += 1;
-            console.log(`You lose! Because ${computerChoice} beats ${humanChoice}`);
+            roundResult.textContent =`You lose! Because ${computerChoice} beats ${humanChoice}`;
+            divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
         }  else {
-            console.log("It's a tie");
+            roundResult.textContent ="It's a tie";
+            divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
         }
     }
 
     else if (humanChoice === 'scissors') {
         if (computerChoice == 'paper') {
             humanScore += 1;
-            console.log(`You win! Because ${humanChoice} beats ${computerChoice}`);
+            roundResult.textContent =`You win! Because ${humanChoice} beats ${computerChoice}`;
+            divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
         }  else if (computerChoice == 'rock') {
             computerScore += 1;
-            console.log(`You lose! Because ${computerChoice} beats ${humanChoice}`);
+            roundResult.textContent =`You lose! Because ${computerChoice} beats ${humanChoice}`;
+            divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
         }  else {
-            console.log("It's a tie");
+            roundResult.textContent ="It's a tie";
+            divList.appendChild(roundResult);
+            scoreboard.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
         }
     }
 }
